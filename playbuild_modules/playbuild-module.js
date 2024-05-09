aa3a22325d3df09c4b57891c26c9f49d32ddc4f8
nasitra <nasitra@users.noreply.github.com>
2024-05-09 21:20:58 +0900

    Use ES modules for PlayBuild modules

--- playbuild_modules/playbuild-module.js
+++ playbuild_modules/playbuild-module.js
@@ -1,10 +1,6 @@
-(function(window) {
-  'use strict';
-
-  Object.defineProperty(window, 'playbuild', {
-    value: Object.create(Object.prototype, {
-      Module: { value: window.parent.PlayBuildModule },
-      exports: { value: null, writable: true },
-    }),
-  });
-})(this);
+Object.defineProperty(window, 'playbuild', {
+  value: Object.create(Object.prototype, {
+    Module: { value: window.parent.PlayBuildModule },
+    exports: { value: null, writable: true },
+  }),
+});
