8ff70b837bceae4ec29e560dbf791cf77cb5a388
nasitra <nasitra@users.noreply.github.com>
2024-05-10 18:28:05 +0900

    Update 'file-saver' module

--- js/bundle/file-saver.js
+++ js/bundle/file-saver.js
@@ -3,139 +3,85 @@ var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 
-// node_modules/file-saver/FileSaver.js
-var require_FileSaver = __commonJS({
-  "node_modules/file-saver/FileSaver.js"(exports, module) {
-    var saveAs = saveAs || function(view) {
+// node_modules/file-saver/dist/FileSaver.min.js
+var require_FileSaver_min = __commonJS({
+  "node_modules/file-saver/dist/FileSaver.min.js"(exports, module) {
+    (function(a, b) {
+      if ("function" == typeof define && define.amd) define([], b);
+      else if ("undefined" != typeof exports) b();
+      else {
+        b(), a.FileSaver = { exports: {} }.exports;
+      }
+    })(exports, function() {
       "use strict";
-      if (typeof view === "undefined" || typeof navigator !== "undefined" && /MSIE [1-9]\./.test(navigator.userAgent)) {
-        return;
+      function b(a2, b2) {
+        return "undefined" == typeof b2 ? b2 = { autoBom: false } : "object" != typeof b2 && (console.warn("Deprecated: Expected third argument to be a object"), b2 = { autoBom: !b2 }), b2.autoBom && /^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(a2.type) ? new Blob(["\uFEFF", a2], { type: a2.type }) : a2;
       }
-      var doc = view.document, get_URL = function() {
-        return view.URL || view.webkitURL || view;
-      }, save_link = doc.createElementNS("http://www.w3.org/1999/xhtml", "a"), can_use_save_link = "download" in save_link, click = function(node) {
-        var event = new MouseEvent("click");
-        node.dispatchEvent(event);
-      }, is_safari = /constructor/i.test(view.HTMLElement) || view.safari, is_chrome_ios = /CriOS\/[\d]+/.test(navigator.userAgent), throw_outside = function(ex) {
-        (view.setImmediate || view.setTimeout)(function() {
-          throw ex;
-        }, 0);
-      }, force_saveable_type = "application/octet-stream", arbitrary_revoke_timeout = 1e3 * 40, revoke = function(file) {
-        var revoker = function() {
-          if (typeof file === "string") {
-            get_URL().revokeObjectURL(file);
-          } else {
-            file.remove();
-          }
-        };
-        setTimeout(revoker, arbitrary_revoke_timeout);
-      }, dispatch = function(filesaver, event_types, event) {
-        event_types = [].concat(event_types);
-        var i = event_types.length;
-        while (i--) {
-          var listener = filesaver["on" + event_types[i]];
-          if (typeof listener === "function") {
-            try {
-              listener.call(filesaver, event || filesaver);
-            } catch (ex) {
-              throw_outside(ex);
-            }
-          }
+      function c(a2, b2, c2) {
+        var d2 = new XMLHttpRequest();
+        d2.open("GET", a2), d2.responseType = "blob", d2.onload = function() {
+          g(d2.response, b2, c2);
+        }, d2.onerror = function() {
+          console.error("could not download file");
+        }, d2.send();
+      }
+      function d(a2) {
+        var b2 = new XMLHttpRequest();
+        b2.open("HEAD", a2, false);
+        try {
+          b2.send();
+        } catch (a3) {
         }
-      }, auto_bom = function(blob) {
-        if (/^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(blob.type)) {
-          return new Blob([String.fromCharCode(65279), blob], { type: blob.type });
+        return 200 <= b2.status && 299 >= b2.status;
+      }
+      function e(a2) {
+        try {
+          a2.dispatchEvent(new MouseEvent("click"));
+        } catch (c2) {
+          var b2 = document.createEvent("MouseEvents");
+          b2.initMouseEvent("click", true, true, window, 0, 0, 0, 80, 20, false, false, false, false, 0, null), a2.dispatchEvent(b2);
         }
-        return blob;
-      }, FileSaver2 = function(blob, name, no_auto_bom) {
-        if (!no_auto_bom) {
-          blob = auto_bom(blob);
-        }
-        var filesaver = this, type = blob.type, force = type === force_saveable_type, object_url, dispatch_all = function() {
-          dispatch(filesaver, "writestart progress write writeend".split(" "));
-        }, fs_error = function() {
-          if ((is_chrome_ios || force && is_safari) && view.FileReader) {
-            var reader = new FileReader();
-            reader.onloadend = function() {
-              var url = is_chrome_ios ? reader.result : reader.result.replace(/^data:[^;]*;/, "data:attachment/file;");
-              var popup = view.open(url, "_blank");
-              if (!popup) view.location.href = url;
-              url = void 0;
-              filesaver.readyState = filesaver.DONE;
-              dispatch_all();
-            };
-            reader.readAsDataURL(blob);
-            filesaver.readyState = filesaver.INIT;
-            return;
-          }
-          if (!object_url) {
-            object_url = get_URL().createObjectURL(blob);
-          }
-          if (force) {
-            view.location.href = object_url;
-          } else {
-            var opened = view.open(object_url, "_blank");
-            if (!opened) {
-              view.location.href = object_url;
-            }
-          }
-          filesaver.readyState = filesaver.DONE;
-          dispatch_all();
-          revoke(object_url);
-        };
-        filesaver.readyState = filesaver.INIT;
-        if (can_use_save_link) {
-          object_url = get_URL().createObjectURL(blob);
-          setTimeout(function() {
-            save_link.href = object_url;
-            save_link.download = name;
-            click(save_link);
-            dispatch_all();
-            revoke(object_url);
-            filesaver.readyState = filesaver.DONE;
+      }
+      var f = "object" == typeof window && window.window === window ? window : "object" == typeof self && self.self === self ? self : "object" == typeof global && global.global === global ? global : void 0, a = f.navigator && /Macintosh/.test(navigator.userAgent) && /AppleWebKit/.test(navigator.userAgent) && !/Safari/.test(navigator.userAgent), g = f.saveAs || ("object" != typeof window || window !== f ? function() {
+      } : "download" in HTMLAnchorElement.prototype && !a ? function(b2, g2, h) {
+        var i = f.URL || f.webkitURL, j = document.createElement("a");
+        g2 = g2 || b2.name || "download", j.download = g2, j.rel = "noopener", "string" == typeof b2 ? (j.href = b2, j.origin === location.origin ? e(j) : d(j.href) ? c(b2, g2, h) : e(j, j.target = "_blank")) : (j.href = i.createObjectURL(b2), setTimeout(function() {
+          i.revokeObjectURL(j.href);
+        }, 4e4), setTimeout(function() {
+          e(j);
+        }, 0));
+      } : "msSaveOrOpenBlob" in navigator ? function(f2, g2, h) {
+        if (g2 = g2 || f2.name || "download", "string" != typeof f2) navigator.msSaveOrOpenBlob(b(f2, h), g2);
+        else if (d(f2)) c(f2, g2, h);
+        else {
+          var i = document.createElement("a");
+          i.href = f2, i.target = "_blank", setTimeout(function() {
+            e(i);
           });
-          return;
         }
-        fs_error();
-      }, FS_proto = FileSaver2.prototype, saveAs2 = function(blob, name, no_auto_bom) {
-        return new FileSaver2(blob, name || blob.name || "download", no_auto_bom);
-      };
-      if (typeof navigator !== "undefined" && navigator.msSaveOrOpenBlob) {
-        return function(blob, name, no_auto_bom) {
-          name = name || blob.name || "download";
-          if (!no_auto_bom) {
-            blob = auto_bom(blob);
-          }
-          return navigator.msSaveOrOpenBlob(blob, name);
-        };
-      }
-      FS_proto.abort = function() {
-      };
-      FS_proto.readyState = FS_proto.INIT = 0;
-      FS_proto.WRITING = 1;
-      FS_proto.DONE = 2;
-      FS_proto.error = FS_proto.onwritestart = FS_proto.onprogress = FS_proto.onwrite = FS_proto.onabort = FS_proto.onerror = FS_proto.onwriteend = null;
-      return saveAs2;
-    }(
-      typeof self !== "undefined" && self || typeof window !== "undefined" && window || exports.content
-    );
-    if (typeof module !== "undefined" && module.exports) {
-      module.exports.saveAs = saveAs;
-    } else if (typeof define !== "undefined" && define !== null && define.amd !== null) {
-      define("FileSaver.js", function() {
-        return saveAs;
+      } : function(b2, d2, e2, g2) {
+        if (g2 = g2 || open("", "_blank"), g2 && (g2.document.title = g2.document.body.innerText = "downloading..."), "string" == typeof b2) return c(b2, d2, e2);
+        var h = "application/octet-stream" === b2.type, i = /constructor/i.test(f.HTMLElement) || f.safari, j = /CriOS\/[\d]+/.test(navigator.userAgent);
+        if ((j || h && i || a) && "undefined" != typeof FileReader) {
+          var k = new FileReader();
+          k.onloadend = function() {
+            var a2 = k.result;
+            a2 = j ? a2 : a2.replace(/^data:[^;]*;/, "data:attachment/file;"), g2 ? g2.location.href = a2 : location = a2, g2 = null;
+          }, k.readAsDataURL(b2);
+        } else {
+          var l = f.URL || f.webkitURL, m = l.createObjectURL(b2);
+          g2 ? g2.location = m : location.href = m, g2 = null, setTimeout(function() {
+            l.revokeObjectURL(m);
+          }, 4e4);
+        }
       });
-    }
+      f.saveAs = g.saveAs = g, "undefined" != typeof module && (module.exports = g);
+    });
   }
 });
 
 // js/bundle/base/file-saver.js
-var FileSaver = require_FileSaver();
+var FileSaver = require_FileSaver_min();
 export {
   FileSaver
 };
-/*! Bundled license information:
-
-file-saver/FileSaver.js:
-  (*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/FileSaver.js *)
-*/
