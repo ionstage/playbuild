2901958a88aff9c0984cab7b4e2af8bc78d6a0f9
nasitra <nasitra@users.noreply.github.com>
2024-06-08 11:55:22 +0900

    Refactor code of 'Command'

--- js/models/command.js
+++ js/models/command.js
@@ -1,11 +1,11 @@
 export class Command {
   static parse(s) {
-    const tokens = tokenize(s);
-    const nodes = makeNodes(tokens);
-    if (!isValidNodes(nodes)) {
+    const tokens = Command._tokenize(s);
+    const nodes = Command._makeNodes(tokens);
+    if (!Command._isValidNodes(nodes)) {
       throw new SyntaxError('PlayBuildScript parse error: Unexpected identifier "' +  s + '"');
     }
-    return makeArgs(nodes);
+    return Command._makeArgs(nodes);
   }
 
   static parseList(s) {
@@ -31,62 +31,62 @@ export class Command {
       return s.substring((i !== 0 ? indexes[i-1] + 1 : 0), index);
     });
   }
-}
 
-function tokenize(s) {
-  const tokens = s.match(/".*?[^\\]"|'.*?[^\\]'|\.|#|:|>>|<<|[\w"'/\\]+|\S+/g) || [];
-  const index = tokens.indexOf('#');
-  return (index !== -1 ? tokens.slice(0, index) : tokens);
-}
-
-function makeNodes(tokens) {
-  const nodes = tokens.slice();
-  if (nodes[0] === ':' && nodes.length >= 2) {
-    nodes.shift();
-    nodes[0] = ':' + nodes[0].toLowerCase();
-  } else if (nodes[1] === ':') {
-    nodes.splice(1, 1);
-    nodes.unshift(':new');
-  } else if (nodes[1] === '.' && nodes[3] === '>>' && nodes[5] === '.') {
-    nodes.splice(3, 1);
-    nodes.unshift(':bind');
-  } else if (nodes[1] === '.' && nodes[3] === '<<') {
-    nodes.splice(3, 1);
-    nodes.unshift(':send');
+  static _tokenize(s) {
+    const tokens = s.match(/".*?[^\\]"|'.*?[^\\]'|\.|#|:|>>|<<|[\w"'/\\]+|\S+/g) || [];
+    const index = tokens.indexOf('#');
+    return (index !== -1 ? tokens.slice(0, index) : tokens);
   }
-  return nodes;
-}
-
-function isValidNodes(nodes) {
-  switch (nodes[0]) {
-    case ':new':
-      return (nodes.length >= 3 && nodes.length <= 4 && /^[a-zA-Z]/.test(nodes[1]) && /^[a-zA-Z]/.test(nodes[2]));
-    case ':bind':
-    case ':unbind':
-      return (nodes.length === 7 && nodes[2] === '.' && nodes[5] === '.');
-    case ':send':
-      return (nodes.length >= 4 && nodes.length <= 5 && nodes[2] === '.');
-    case ':delete':
-      return (nodes.length === 2 && /^[a-zA-Z]/.test(nodes[1]));
-    case ':reset':
-      return (nodes.length === 1);
-    case ':load':
-    case ':save':
-      return (nodes.length <= 2);
-    default:
-      return (nodes.length === 0);
-  }
-}
-
-function makeArgs(nodes) {
-  return nodes.filter(node => {
-    return (node !== '.');
-  }).map(node => {
-    const first = node.charAt(0);
-    const last = node.slice(-1);
-    if (first === last && (first === '\'' || first === '"')) {
-      node = node.slice(1, -1);
+  
+  static _makeNodes(tokens) {
+    const nodes = tokens.slice();
+    if (nodes[0] === ':' && nodes.length >= 2) {
+      nodes.shift();
+      nodes[0] = ':' + nodes[0].toLowerCase();
+    } else if (nodes[1] === ':') {
+      nodes.splice(1, 1);
+      nodes.unshift(':new');
+    } else if (nodes[1] === '.' && nodes[3] === '>>' && nodes[5] === '.') {
+      nodes.splice(3, 1);
+      nodes.unshift(':bind');
+    } else if (nodes[1] === '.' && nodes[3] === '<<') {
+      nodes.splice(3, 1);
+      nodes.unshift(':send');
     }
-    return node.replace(/\\(["'])/g, '$1');
-  });
+    return nodes;
+  }
+  
+  static _isValidNodes(nodes) {
+    switch (nodes[0]) {
+      case ':new':
+        return (nodes.length >= 3 && nodes.length <= 4 && /^[a-zA-Z]/.test(nodes[1]) && /^[a-zA-Z]/.test(nodes[2]));
+      case ':bind':
+      case ':unbind':
+        return (nodes.length === 7 && nodes[2] === '.' && nodes[5] === '.');
+      case ':send':
+        return (nodes.length >= 4 && nodes.length <= 5 && nodes[2] === '.');
+      case ':delete':
+        return (nodes.length === 2 && /^[a-zA-Z]/.test(nodes[1]));
+      case ':reset':
+        return (nodes.length === 1);
+      case ':load':
+      case ':save':
+        return (nodes.length <= 2);
+      default:
+        return (nodes.length === 0);
+    }
+  }
+  
+  static _makeArgs(nodes) {
+    return nodes.filter(node => {
+      return (node !== '.');
+    }).map(node => {
+      const first = node.charAt(0);
+      const last = node.slice(-1);
+      if (first === last && (first === '\'' || first === '"')) {
+        node = node.slice(1, -1);
+      }
+      return node.replace(/\\(["'])/g, '$1');
+    });
+  }
 }
